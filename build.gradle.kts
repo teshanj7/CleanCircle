1c57e5d2576b8d76ddbdf51054ffb4aa241fb2b2
ManulaGunatilleke <inner.gunatilleke@gmail.com>
2023-10-21 13:50:46 +0530

    push updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+    id ("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
